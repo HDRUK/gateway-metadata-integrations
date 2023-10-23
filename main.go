14101805e483db84363f9e065a970349ba05630b
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2023-10-23 16:20:50 +0100

    remove run code for now

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"hdruk/federated-metadata/pkg/pull"
 	"hdruk/federated-metadata/pkg/push"
 	"time"
 
@@ -25,9 +24,9 @@ func main() {
 
 	// TODO - reinstate this once we have federations
 	// to begin running.
-	scheduler.Every(1).Minute().Do(func() {
-		pull.Run()
-	})
+	// scheduler.Every(1).Minute().Do(func() {
+	// 	pull.Run()
+	// })
 
 	scheduler.StartBlocking()
 }
